973366ffbfea12400981af8268aed932560043a9
Twilight Sparkle <19155609+Twi1ightSparkle@users.noreply.github.com>
2023-02-27 17:21:32 +0000

    Init commit

---
+++ README.md
@@ -0,0 +1,25 @@
+<!--
+    Renice applications to 20
+    Copyright (C) 2023 Twilight Sparkle
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
+
+# Renice applications to 20
+
+Sets the niceness value for all applications to the lowest possible priority.
+
+Copy `apps.example.sh` to `apps.sh`. Add your application names. Run `sudo ./nice.sh` to list app applications that
+will be affected. Run `sudo ./nice.sh --live` to apply new niceness values.
+
---
+++ nice.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Renice applications to 20
+# Copyright (C) 2023 Twilight Sparkle
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+source ./apps.sh
+
+if [ "$UID" != 0 ]; then
+    echo "This script must be ran as root"
+    exit 1
+fi
+
+pids=()
+
+for app in "${applications[@]}"; do
+    for pid in $(pgrep -fi "$app"); do
+        pids+=("$pid")
+    done
+done
+
+if [ "$1" != "--live" ]; then
+    echo "Running in dry-run mode. Pass --live to renice apps"
+
+    for pid in "${pids[@]}"; do
+        ps -p "$pid"
+    done
+else
+    echo "Running in live mode, renicing applications"
+
+    for pid in "${pids[@]}"; do
+        renice 20 "$pid"
+    done
+fi
+
